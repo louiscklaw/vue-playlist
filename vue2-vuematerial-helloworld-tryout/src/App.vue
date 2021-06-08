27d774ed184c566227e5d4b43e966a45666c2818
louiscklaw <louiscklaw@gmail.com>
2021-06-09 01:11:51 +0800

    update and test,

---
+++ vue2-vuematerial-helloworld-tryout/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+Vue.use(VueMaterial);
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <HelloWorld msg="helloworld ?" />
+    <MdButton>helloworld</MdButton>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
