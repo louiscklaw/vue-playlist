8161cc66af7dc2c7603ecd777caf439099d5ca54
louiscklaw <louiscklaw@gmail.com>
2021-06-14 12:01:15 +0800

    update and test,

---
+++ vue2-formulate-tryout/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    helloworld
+    <div>
+      FormulateInput
+    </div>
+    <div>
+      <FormulateInput type="text" />
+    </div>
+
+    <div>
+      FormulateInput textarea
+    </div>
+    <div>
+      <FormulateInput type="textarea" />
+    </div>
+
+    <div>
+      FormulateInput email
+    </div>
+    <div>
+      <FormulateInput type="email" name="email" validation="required|email" />
+    </div>
+    <div>
+      <FormulateInput type="text" label="What is your name?" v-model="value" />
+      <strong
+        >Hello <em>{{ value }}</em
+        >!</strong
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld,
+  // },
+  data() {
+    return {
+      value: 'My initial value',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
