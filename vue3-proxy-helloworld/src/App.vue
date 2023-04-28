29e2eca18a67f75b6255f06f3dbdb20823ed1fb0
louiscklaw <louiscklaw@users.noreply.github.com>
2023-04-28 14:34:34 +0800

    update,

---
+++ vue3-proxy-helloworld/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  helloworld
+</template>
+
+<style scoped>
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+</style>
