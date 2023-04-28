1a9eb1941c450821d6b5776226efda53fcd097a5
louiscklaw <louiscklaw@users.noreply.github.com>
2023-04-28 14:32:33 +0800

    update vue3-helloworld,

---
+++ vue3-input-helloworld-tryout/notes.md
@@ -0,0 +1,14 @@
+```bash
+✔ Project name: … <your-project-name>
+✔ Add TypeScript? … No / Yes
+✔ Add JSX Support? … No / Yes
+✔ Add Vue Router for Single Page Application development? … No / Yes
+✔ Add Pinia for state management? … No / Yes
+✔ Add Vitest for Unit testing? … No / Yes
+✔ Add Cypress for both Unit and End-to-End testing? … No / Yes
+✔ Add ESLint for code quality? … No / Yes
+✔ Add Prettier for code formatting? … No / Yes
+
+Scaffolding project in ./<your-project-name>...
+Done.
+```
---
+++ vue3-input-helloworld-tryout/src/App.vue
@@ -0,0 +1,107 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+// import TheWelcome from './components/TheWelcome.vue';
+
+export default {
+  data() {
+    return {
+      msg: '',
+      message: '',
+      checked: false,
+      checkedNames: [],
+      selected: 'A',
+      options: [
+        { text: 'One', value: 'A' },
+        { text: 'Two', value: 'B' },
+        { text: 'Three', value: 'C' },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div>helloworld</div>
+
+  <p>Message is: {{ message }}</p>
+  <input v-model="message" placeholder="edit me" />
+
+  <span>Multiline message is:</span>
+  <p style="white-space: pre-line">{{ long_message }}</p>
+  <textarea v-model="long_message" placeholder="add multiple lines"></textarea>
+
+  <textarea v-model="text"></textarea>
+
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">{{ checked }}</label>
+
+  <div>
+    <div>Checked names: {{ checkedNames }}</div>
+
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+    <label for="jack">Jack</label>
+
+    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+    <label for="john">John</label>
+
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+    <label for="mike">Mike</label>
+  </div>
+
+  <div>
+    <div>Selected: {{ selected }}</div>
+
+    <select v-model="selected">
+      <option disabled value="">Please select one</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+  </div>
+
+  <div>
+    <div>Selected: {{ selected }}</div>
+
+    <select v-model="selected" multiple>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+  </div>
+
+  <div>
+    <select v-model="selected">
+      <option v-for="option in options" :value="option.value">
+        {{ option.text }}
+      </option>
+    </select>
+
+    <div>Selected: {{ selected }}</div>
+  </div>
+
+  <!-- Value Bindings -->
+  <div>
+    <!-- `picked` is a string "a" when checked -->
+    <input type="radio" v-model="picked" value="a" />
+
+    <!-- `toggle` is either true or false -->
+    <input type="checkbox" v-model="toggle" />
+
+    <!-- `selected` is a string "abc" when the first option is selected -->
+    <select v-model="selected">
+      <option value="abc">ABC</option>
+    </select>
+  </div>
+
+  <div>
+    <input v-model.lazy="msg" />
+  </div>
+</template>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
