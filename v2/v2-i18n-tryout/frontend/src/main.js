9e37fa84470dedaee24c9595f73707f822c7c18f
Louis Law <louiscklaw@users.noreply.github.com>
2023-11-17 19:44:02 +0800

    update,

--- v2/v2-i18n-tryout/frontend/src/main.js
+++ v2/v2-i18n-tryout/frontend/src/main.js
@@ -3,19 +3,14 @@ import VueI18n from 'vue-i18n';
 
 import App from './App.vue';
 
+import en from './locale/en';
+import ja from './locale/ja';
+
 Vue.use(VueI18n);
 
 const messages = {
-  en: {
-    message: {
-      hello: 'hello world',
-    },
-  },
-  ja: {
-    message: {
-      hello: 'こんにちは、世界',
-    },
-  },
+  en,
+  ja,
 };
 
 const i18n = new VueI18n({
