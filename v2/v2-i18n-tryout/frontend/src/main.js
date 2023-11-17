c332f2aa975fc20a0e294c8cf3d45fdb5888c5a3
Louis Law <louiscklaw@users.noreply.github.com>
2023-11-17 19:30:57 +0800

    update,

--- v2/v2-i18n-tryout/frontend/src/main.js
+++ v2/v2-i18n-tryout/frontend/src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+
+Vue.use(VueI18n);
+
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world',
+    },
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界',
+    },
+  },
+};
+
+const i18n = new VueI18n({
+  locale: 'ja', // set locale
+  messages, // set locale messages
+});
 
 new Vue({
+  i18n,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+Vue.config.productionTip = false;
