eb7e4a7f80a507d947bb050aac593e947181cfef
Louis Law <louiscklaw@users.noreply.github.com>
2023-11-17 19:04:04 +0800

    update,

--- v2/v2-i18n-tryout/frontend/src/App.vue
+++ v2/v2-i18n-tryout/frontend/src/App.vue
@@ -1,21 +1,19 @@
-<script src="https://unpkg.com/vue-i18n@8"></script>
-
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
 <style>
